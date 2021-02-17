4e4eedf8d5170b265b793c68d214a5a43a9c3b01
maszuky <79188577+maszuky@users.noreply.github.com>
2021-02-17 13:55:27 +0700

    Add

---
+++ main.js
@@ -0,0 +1,95 @@
+const electron = require('electron')
+const { 
+  app,  
+  dialog,
+  Menu,
+  MenuItem,
+  ipcMain,
+  globalShortcut, 
+  BrowserWindow
+} = electron
+
+let mainWindow
+const display = 'http://assalamcgl.com/front';
+const ipcam = 'http://192.168.1.160:8080/jsfs.html';
+const path = require('path')
+const modalPath = path.join('file://', __dirname, 'index.html')
+
+app.on('ready', () => {
+  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
+  mainWindow = new BrowserWindow({
+    fullscreen: true,
+    frame: false
+  })
+  mainWindow.loadURL(display)
+})
+
+// Fungsi Menu klik kanan
+const menu = new Menu()
+menu.append(new MenuItem({
+  label: 'Diplay (CTRL + X)',
+  click() {
+    mainWindow.loadURL(display)
+    mainWindow.show()
+  }
+}))
+
+menu.append(new MenuItem({
+  label: 'Live Video (CTRL + L)',
+  click() {
+    mainWindow.loadURL(ipcam)
+    mainWindow.show()
+  }
+}))
+
+menu.append(new MenuItem({ type: 'separator' }))
+menu.append(new MenuItem({
+  label: 'Pengaturan (CTRL + P)',
+  click() {
+    mainWindow.loadURL(modalPath)
+    mainWindow.show()
+  }
+}))
+
+menu.append(new MenuItem({
+  label: 'Keluar (ESC)',
+  click() {
+    mainWindow.close();
+  }
+}))
+
+app.on('browser-window-created', (event, winMenu) => {
+  winMenu.webContents.on('context-menu', (e, params) => {
+    menu.popup(winMenu, params.x, params.y)
+  })
+})
+
+ipcMain.on('show-context-menu', (event) => {
+  const winMenu = BrowserWindow.fromWebContents(event.sender)
+  menu.popup(winMenu)
+})
+
+// Fungsi untuk keypress keyboard
+app.on('ready', () => {
+  globalShortcut.register('Esc', () => {
+    mainWindow.close();
+  })
+})
+app.on('ready', () => {
+  globalShortcut.register('CommandOrControl+x', () => {
+    mainWindow.loadURL(display)
+  })
+})
+app.on('ready', () => {
+  globalShortcut.register('CommandOrControl+l', () => {
+    mainWindow.loadURL(ipcam)
+  })
+})
+app.on('ready', () => {
+  globalShortcut.register('CommandOrControl+p', () => {
+    mainWindow.loadURL(modalPath)
+  })
+})
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll()
+})
